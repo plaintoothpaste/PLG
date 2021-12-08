321147426937db05515ffd0c1c3d317293d3eb49
plaintootpaste <guica.matthew@gmail.com>
2021-12-08 22:57:30 +0100

    Added license in a way code ocean likes Getting it all to work with code ocean where the results dir is cleared every time.

---
+++ code/runCodeOcean.m
@@ -0,0 +1,9 @@
+% This is a special script only on the code ocean branch that runs all
+% demos, tests and generates unit cell images.
+runTests();
+
+runDemos();
+
+addpath('../data');
+plotUnitCell();
+addpath('../data');
